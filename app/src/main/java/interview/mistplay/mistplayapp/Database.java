3ada7325d1a6ce9ca44bd4376e5ab15492be2492
keithnicholas <keithnicholas210@gmail.com>
2019-06-26 15:49:44 -0700

    Last Commit

--- app/src/main/java/interview/mistplay/mistplayapp/Database.java
+++ app/src/main/java/interview/mistplay/mistplayapp/Database.java
@@ -1,6 +1,93 @@
 package interview.mistplay.mistplayapp;
 
-public class Database {
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Comparator;
+
+import okhttp3.HttpUrl;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+public class Database {
+    Game[] gamelist;
+    ObjectMapper objectMapper;
+    final int MAX_NUM = 15;
+    int currentIndex =0;
+    public Database() throws Exception{
+        objectMapper = new ObjectMapper();
+    }
+    public Game[] search(String query) throws Exception{
+        Game[] gameListReturned;
+        class RatingComparator implements Comparator<Game> {
+            public int compare(Game m1, Game m2) {
+                return (m2.getRating().compareTo(m1.getRating()));
+            }
+        }
+
+        //checks if user are only entering 1 words (meaning searching by subgenre)
+        if(query.split("\\s+").length == 1){
+            searchSubgenre(query);
+            Arrays.sort(gamelist,new RatingComparator());
+            gameListReturned = Arrays.copyOfRange(gamelist, currentIndex, MAX_NUM);
+            currentIndex = MAX_NUM +1;
+        }else{ //search for title
+            searchTitle(query);
+            Arrays.sort(gamelist,new RatingComparator());
+            if(gamelist.length <1){ //no result found for title
+
+            }
+            gameListReturned = Arrays.copyOfRange(gamelist, currentIndex, MAX_NUM);
+            currentIndex = MAX_NUM +1;
+        }
+        //if search by pure title returns nothing
+
+        return gameListReturned;
+
+    }
+    /**
+     * Search apps based on title given a query
+     * @param query
+     * @throws Exception
+     */
+    private void searchTitle(String query) throws Exception{
+        String urls="http://localhost:8080/search";
+        OkHttpClient client = new OkHttpClient();
+
+        HttpUrl.Builder urlBuilder = HttpUrl.parse(urls).newBuilder();
+        urlBuilder.addQueryParameter("subgenre", query);
+        String url = urlBuilder.build().toString();
+
+        Request request = new Request.Builder()
+                .url(url)
+                .build();
+
+        Response response = client.newCall(request).execute();
+        gamelist = objectMapper.readValue(response.body().toString(), Game[].class);
+    }
+
+    /**
+     * Search apps based on subgenre given a query
+     * @param query
+     * @throws Exception
+     */
+    private void searchSubgenre(String query) throws  Exception{
+        String urls="http://localhost:8080/search";
+        OkHttpClient client = new OkHttpClient();
+
+        HttpUrl.Builder urlBuilder = HttpUrl.parse(urls).newBuilder();
+        urlBuilder.addQueryParameter("subgenre", query);
+        String url = urlBuilder.build().toString();
+
+        Request request = new Request.Builder()
+                .url(url)
+                .build();
+        Response response = client.newCall(request).execute();
+        gamelist = objectMapper.readValue(response.body().toString(), Game[].class);
+
+    }
 
 }
--- app/src/main/java/interview/mistplay/mistplayapp/MainActivity.java
+++ app/src/main/java/interview/mistplay/mistplayapp/MainActivity.java
@@ -7,17 +7,22 @@ import android.text.TextWatcher;
 import android.widget.EditText;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import com.mancj.materialsearchbar.MaterialSearchBar;
 
+
+
+public class MainActivity extends AppCompatActivity {
+    Database database;
+    private MaterialSearchBar searchBar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        searchBar = (MaterialSearchBar) findViewById(R.id.searchbar);
+        searchBar.disableSearch();
 
 
-        EditText field = (EditText) findViewById(R.id.field);
-
-        field.addTextChangedListener(new TextWatcher() {
+        searchBar.addTextChangeListener(new TextWatcher() {
 
             // the user's changes are saved here
             public void onTextChanged(CharSequence c, int start, int before, int count) {
@@ -27,10 +32,28 @@ public class MainActivity extends AppCompatActivity {
             public void beforeTextChanged(CharSequence c, int start, int count, int after) {
                 // this space intentionally left blank
             }
-
+            //During text changed, update query and list subsequently
             public void afterTextChanged(Editable c) {
-                TextView textView = (TextView) findViewById(R.id.textView);
-                textView.setText(c.toString());
+                try {
+                    database=new Database();
+                    TextView textView = (TextView) findViewById(R.id.editText);
+                    //textView.setText(c.toString());
+
+                    Game[] game ;
+                    game = database.search(c.toString());
+
+                    String name="";
+                    /*
+                    Test if query runs (TEMPLATE)
+                     */
+                    for(Game g: game){
+                        name += g.getTitle()+" ";
+                    }
+                    textView.setText(name);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+
             }
         });
 
